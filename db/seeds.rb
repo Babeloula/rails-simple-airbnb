369ceff719dc653912f0bf29d43a99ff53702c2f
Walid Benzina <walid.benzina@yahoo.fr>
2021-08-11 20:38:51 +0200

    Create Rails project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+CATEGORY = %w(chinese italian japanese french belgian)
+
+10.times do
+  post = Flat.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.most_interesting_man_in_the_world,
+    price_per_night: rand(500),
+    number_of_guests: rand(400)
+  )
+  post.save!
+end
